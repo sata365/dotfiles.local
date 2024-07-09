706d6ce088293ffb42baf5cb2a1528ba125f4a6e
sata365 <9369075+sata365@users.noreply.github.com>
2024-07-09 14:46:27 +0900

    update

--- macOS/.bin/resources.sh
+++ macOS/.bin/resources.sh
@@ -15,4 +15,12 @@ ln -s ${LOCAL_DOTFILES_REPO_DIR}/macOS/.resources/git/.gitconfig_local ~/.gitcon
 ln -s ${LOCAL_DOTFILES_REPO_DIR}/macOS/.resources/ssh/config ~/.ssh/config
 COMMENTOUT
 
+# zsh
+[ -L ~/.zprofile.local ] && unlink ~/.zprofile.local
+[ -f ~/.zprofile.local ] && cp ~/.zprofile.local ~/.dotbackup/ && rm -f ~/.zprofile.local
+ln -s ${LOCAL_DOTFILES_REPO_DIR}/macOS/.resources/zsh/.zprofile.local ~/.zprofile.local
+[ -L ~/.zshrc.local ] && unlink ~/.zshrc.local
+[ -f ~/.zshrc.local ] && cp ~/.zshrc.local ~/.dotbackup/ && rm -f ~/.zshrc.local
+ln -s ${LOCAL_DOTFILES_REPO_DIR}/macOS/.resources/zsh/.zshrc.local ~/.zshrc.local
+
 echo "------- Finish resource file operation."
